fa4c8ea2badb55aac6d760d643257171ebbb8e06
rootalam <88923083+rootalam@users.noreply.github.com>
2022-07-16 10:44:42 +0530

    Authored

---
+++ gui_rdp_centos_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##################################################################################
+#                                                                                #
+# Run this server if you have reboot after completion of first script.           #
+#                                                                                #
+# Author: Jahangeer Alam                                                         #
+#                                                                                #
+##################################################################################
+
+sudo yum install -y epel-release
+sudo yum -y install xrdp tigervnc-server
+sudo systemctl start xrdp
+sudo netstat -antup | grep xrdp
+sudo systemctl enable xrdp
+
+echo "RDP is installed and running on default port"
+echo "To customize RDP port make change in /etc/xrdp/xrdp.ini file"
+echo "Now create an user, set password and login with RDP client"
